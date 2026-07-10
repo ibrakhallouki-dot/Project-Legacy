ba28f99f87859f501b82a4b36c443def0f512c46
ibrakhallouki-dot <ibrakhallouki@gmail.com>
2026-07-10 22:07:25 +0100

    Update GameScene.ts

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,13 +1,10 @@
 import Phaser from "phaser";
+import { Player } from "../entities/Player";
 import { Learner } from "../entities/Learner";
 
 export class GameScene extends Phaser.Scene {
 
-    private player!: Phaser.GameObjects.Rectangle;
-
-    private playerBody!: Phaser.Physics.Arcade.Body;
-
-    private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+    private player!: Player;
 
     private learners: Learner[] = [];
 
@@ -26,32 +23,31 @@ export class GameScene extends Phaser.Scene {
         graphics.lineStyle(1, 0x222222);
 
         for (let x = 0; x <= 3000; x += 64) {
+
             graphics.moveTo(x, 0);
             graphics.lineTo(x, 3000);
+
         }
 
         for (let y = 0; y <= 3000; y += 64) {
+
             graphics.moveTo(0, y);
             graphics.lineTo(3000, y);
+
         }
 
         graphics.strokePath();
 
-        this.player = this.add.rectangle(
+        this.player = new Player(
+            this,
             1500,
-            1500,
-            32,
-            32,
-            0x00ff66
+            1500
         );
 
-        this.physics.add.existing(this.player);
-
-        this.playerBody = this.player.body as Phaser.Physics.Arcade.Body;
-
-        this.playerBody.setCollideWorldBounds(true);
-
-        this.cameras.main.startFollow(this.player);
+        this.cameras.main.startFollow(
+            this.player.sprite,
+            true
+        );
 
         this.cameras.main.setBounds(
             0,
@@ -60,22 +56,21 @@ export class GameScene extends Phaser.Scene {
             3000
         );
 
-        this.cursors =
-            this.input.keyboard!.createCursorKeys();
-
         for (let i = 0; i < 20; i++) {
 
-            const learner = new Learner(
+            this.learners.push(
 
-                this,
+                new Learner(
 
-                Phaser.Math.Between(100, 2900),
+                    this,
 
-                Phaser.Math.Between(100, 2900)
+                    Phaser.Math.Between(100, 2900),
+
+                    Phaser.Math.Between(100, 2900)
+
+                )
 
             );
-
-            this.learners.push(learner);
 
         }
 
@@ -83,37 +78,19 @@ export class GameScene extends Phaser.Scene {
 
     update(time: number, delta: number) {
 
-        this.playerBody.setVelocity(0);
-
-        const speed = 220;
-
-        if (this.cursors.left.isDown) {
-
-            this.playerBody.setVelocityX(-speed);
-
-        }
-
-        else if (this.cursors.right.isDown) {
-
-            this.playerBody.setVelocityX(speed);
-
-        }
-
-        if (this.cursors.up.isDown) {
-
-            this.playerBody.setVelocityY(-speed);
-
-        }
-
-        else if (this.cursors.down.isDown) {
-
-            this.playerBody.setVelocityY(speed);
-
-        }
-
-        this.playerBody.velocity.normalize().scale(speed);
+        this.player.update();
 
         for (const learner of this.learners) {
+
+            learner.observePlayer(
+
+                this.player.sprite.x,
+
+                this.player.sprite.y,
+
+                delta
+
+            );
 
             learner.update(delta);
 
